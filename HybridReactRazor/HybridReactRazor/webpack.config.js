04b4674fb96b3b0dd625bf1ce6d5188a9f9fa605
farhadmpr <farhadmp@gmail.com>
2021-01-28 16:36:07 +0330

    Add webpack.config.js

---
+++ HybridReactRazor/HybridReactRazor/webpack.config.js
@@ -0,0 +1,28 @@
+﻿"use strict";
+
+var path = require("path");
+var WebpackNotifierPlugin = require("webpack-notifier");
+var BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+
+module.exports = {
+    entry: {
+        home_index: "./Frontend/Home/index.jsx"
+    },
+    output: {
+        path: path.resolve(__dirname, "./wwwroot/js/dist/"),
+        filename: "[name].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    devtool: "inline-source-map",
+    plugins: [new WebpackNotifierPlugin(), new BrowserSyncPlugin()]
+};
