a845a5824bca8e91a2c347de5b0871ccf839a89f
farhadmpr <farhadmp@gmail.com>
2021-01-28 16:55:02 +0330

    Add react index.jsx

---
+++ HybridReactRazor/HybridReactRazor/Frontend/Home/index.jsx
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+    <React.Fragment>
+        <h1>React in ASP.NET MVC!</h1>
+        <div>Hello React World</div>
+    </React.Fragment>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
